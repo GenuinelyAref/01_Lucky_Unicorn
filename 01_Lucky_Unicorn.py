96fd8394a57d6d723564986d395b078f56a523ae
Aref <osmana69529@masseyhigh.school.nz>
2020-02-12 12:35:28 +1300

    Create 01_Lucky_Unicorn.py

---
+++ 01_Lucky_Unicorn.py
@@ -0,0 +1,15 @@
+# 1 Display "Push any key to start"
+
+# 2 Show game information
+
+# 3 Display push 'x' to insert amount of money
+
+# 4 If number doesn't meet criteria
+# a Display "Please enter whole number value between 1 and 10."
+# b Ask for amount of money
+
+# 5 If number value is between 1 and 10, and is only natural numbers, and does not foreign characters (letters):
+# a Proceed to step 6
+
+# 6
+
