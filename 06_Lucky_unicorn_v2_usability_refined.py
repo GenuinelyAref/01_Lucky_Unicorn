9b4de4353229c318ec3cb0dda4df9cb6f3ad5cc6
Aref <osmana69529@masseyhigh.school.nz>
2020-02-19 12:20:28 +1300

    everything so far

--- 06_Lucky_unicorn_v2_usability_refined.py
+++ 06_Lucky_unicorn_v2_usability_refined.py
@@ -2,6 +2,7 @@
 # Program should work - needs to be tested for usability
 
 import random
+Rounds_played = 0
 
 # Integer checking function below
 def intcheck(question, low, high):
@@ -22,13 +23,17 @@ def intcheck(question, low, high):
 
 # Ask user how much they want to play with (min $1, max $10)
 balance = intcheck("\n"
-                   "**** Welcome to the Lucky Unicorn Game **** \n" +
-                   "\n To play, enter an amount of money between $1 and $10 (whole dollars only).\n\n"
+                   "\033[1m\033[44;37m**** Welcome to the Lucky Unicorn Game **** \033[m\033[0m \n" +
+                   "\nThis game is designed to help the charity 'Bricks for India'. All processed money is transferred "
+                   "directly to the non-profit organisation. \n"
+
+                   "A random token is generated each round, and based on it, will be the amount of money you earn.\n"
+                   "\n\033[1mTo play, enter an amount of money between $1 and $10 (whole dollars only). \033[0m\n\n"
                    "- Each round costs $1 \n \n"
-                   "Payouts... \n"
+                   "\033[1m\033[44;46mPayouts... \033[m\n"
                    "- Unicorn: $5.00 \n"
                    "- Horse / Zebra: $0.50 \n"
-                   "- Donkey: $0.00 \n \n \n"
+                   "- Donkey: $0.00 \033[0m\n \n \n"
 
 
                    "How much money would you like to play with?: \n"
@@ -45,32 +50,54 @@ while keep_going == "":
     # Randomly choose a token from our list above
     token = random.choice(tokens)
     print()
-    print("You got a {}".format(token))
 
     # Adjust total correctly for a given token
     if token == "unicorn":
         balance += 5
-        feedback = "Congratulations you won $5.00"
+        Rounds_played += 1
+        feedback = ("####################################################### \n"
+                    "## Congratulations! You got a unicorn. You won $5.00 ## \n"
+                    "####################################################### \n"
+                    "Rounds played: {}".format(Rounds_played) +
+                    "\t \t \t  Balance: ${:.2f}".format(balance)
+                    )
     elif token == "donkey":
         balance -= 1
-        feedback = "Sorry, you did not win anything this round"
+        Rounds_played += 1
+        feedback = ("------------------------------------------------------------------ \n"
+                    "-- Sorry. You got a donkey. You did not win anything this round -- \n"
+                    "------------------------------------------------------------------ \n"
+                    "Rounds played: {}".format(Rounds_played) +
+                    "\t \t \t  Balance: ${:.2f}".format(balance)
+                    )
+    elif token == "horse":
+        balance -= 0.5
+        Rounds_played += 1
+        feedback = ("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ \n"
+                    "^^ Sorry. You got a horse. You won back 50c from your $1 ^^ \n"
+                    "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ \n"
+                    "Rounds played: {}".format(Rounds_played) +
+                    "\t \t \t  Balance: ${:.2f}".format(balance)
+                    )
     else:
         balance -= 0.5
-        feedback = ("------------------------------------------------------------------ \n"
-                    "-- Sorry. you got a donkey. you did not win anything this round -- \n"
-                    "------------------------------------------------------------------ \n \n"
+        Rounds_played += 1
+        feedback = ("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ \n"
+                    "^^ Sorry. You got a horse. You won back 50c from your $1 ^^ \n"
+                    "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ \n"
+                    "Rounds played: {}".format(Rounds_played) +
+                    "\t \t \t  Balance: ${:.2f}".format(balance)
                     )
 
     print()
 
     print(feedback)
-    print("You have ${:.2f} to play with".format(balance))
     print()
 
     if balance < 1:
         print("Sorry you don't enough money to continue. Game over")
         keep_going = "end"
     else:
-        keep_going = input("Press <enter> to play again or any key to quit")
+        keep_going = input("Press <enter> to play again or any key to quit\n\n")
 
 print("Thank you for playing. ")
