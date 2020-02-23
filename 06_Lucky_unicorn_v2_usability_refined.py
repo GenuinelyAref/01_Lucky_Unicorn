fb58723de1243aa80961eb5fb02b8b97bc400a19
jgottschalkmhs <49209767+jgottschalkmhs@users.noreply.github.com>
2020-02-23 16:26:10 +1300

    GK_07_LU_efficient
    This code includes a number of improvements.  Variables such as 'Rounds_played' have been initialised at the start of the main routine AFTER the functions.  The  list containing the tokens has been defined outside of the While loop as it does not need to be defined each time a round is played.
    
    The decoration around the feedback statements is not achieved by using a function (to avoid repeated code) and incrementing the number of rounds played occurs toward the start of the while loop (rather than being coded separately as part of each 'if / elif / else' statement.
    
    From the users' perspective, one of the biggest changes is having the introduction separate from the error message that occurs if users enter invalid data when asked how much they would like to spend.  This game could be further improved by asking users if they have played the game before - if they say 'no', then instructions could be displayed, if they say 'yes', then the game could be launched without the instructions.

--- 06_Lucky_unicorn_v2_usability_refined.py
+++ 06_Lucky_unicorn_v2_usability_refined.py
@@ -2,27 +2,43 @@
 # Program should work - needs to be tested for usability
 
 import random
-Rounds_played = 0
+
 
 # Integer checking function below
 def intcheck(question, low, high):
-        valid = False
-        error = "Whoops! Please enter an integer between {} and {}".format(low, high)
-        while not valid:
-            try:
-                response = int(input(question))
-                if low <= response <= high:
-                    return response
-                else:
-                    print(error)
-                    print()
-            except ValueError:
+    valid = False
+    error = "Whoops! Please enter an integer between {} and {}".format(low, high)
+    while not valid:
+        try:
+            response = int(input(question))
+            if low <= response <= high:
+                return response
+            else:
                 print(error)
                 print()
+        except ValueError:
+            print(error)
+            print()
 
+# Statement Generator
+def statement_gen(statement, decoration):
+    print(decoration * len(statement))
+    print(statement)
+    print(decoration * len(statement))
+          
 
-# Ask user how much they want to play with (min $1, max $10)
-balance = intcheck("\n"
+# Main Routine Goes here...
+
+# Set up Initial variables and lists
+Rounds_played = 0
+
+# tokens list that includes 10 times to prevent too many unicorns being chosen
+tokens = ["horse", "horse", "horse",
+          "zebra", "zebra", "zebra",
+          "donkey", "donkey", "donkey", "unicorn"]
+
+# **** Introduction *****
+print("\n"
                    "\033[1m\033[44;37m**** Welcome to the Lucky Unicorn Game **** \033[m\033[0m \n" +
                    "\nThis game is designed to help the charity 'Bricks for India'. All processed money is transferred "
                    "directly to the non-profit organisation. \n"
@@ -33,65 +49,42 @@ balance = intcheck("\n"
                    "\033[1m\033[44;46mPayouts... \033[m\n"
                    "- Unicorn: $5.00 \n"
                    "- Horse / Zebra: $0.50 \n"
-                   "- Donkey: $0.00 \033[0m\n \n \n"
+                   "- Donkey: $0.00 \033[0m\n \n \n")
 
+print()
 
-                   "How much money would you like to play with?: \n"
-                   "Type here: $ ", 1, 10)
+# Ask user how much they want to play with (min $1, max $10)
+balance = intcheck("How much money would you like to play with?: $ ", 1, 10)
 
 keep_going = ""
 while keep_going == "":
 
-    # tokens list that includes 10 times to prevent too many unicorns being chosen
-    tokens = ["horse", "horse", "horse",
-              "zebra", "zebra", "zebra",
-              "donkey", "donkey", "donkey", "unicorn"]
-
     # Randomly choose a token from our list above
     token = random.choice(tokens)
+    Rounds_played += 1
     print()
 
     # Adjust total correctly for a given token
     if token == "unicorn":
         balance += 5
-        Rounds_played += 1
-        feedback = ("####################################################### \n"
-                    "## Congratulations! You got a unicorn. You won $5.00 ## \n"
-                    "####################################################### \n"
-                    "Rounds played: {}".format(Rounds_played) +
-                    "\t \t \t  Balance: ${:.2f}".format(balance)
-                    )
+        feedback = statement_gen("## Congratulations! You got a unicorn. You won $5.00 ##", "#")       
+                    
     elif token == "donkey":
         balance -= 1
-        Rounds_played += 1
-        feedback = ("------------------------------------------------------------------ \n"
-                    "-- Sorry. You got a donkey. You did not win anything this round -- \n"
-                    "------------------------------------------------------------------ \n"
-                    "Rounds played: {}".format(Rounds_played) +
-                    "\t \t \t  Balance: ${:.2f}".format(balance)
-                    )
+        feedback = statement_gen("-- Sorry. You got a donkey. You did not win anything this round --", "-")       
+
     elif token == "horse":
         balance -= 0.5
-        Rounds_played += 1
-        feedback = ("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ \n"
-                    "^^ Sorry. You got a horse. You won back 50c from your $1 ^^ \n"
-                    "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ \n"
-                    "Rounds played: {}".format(Rounds_played) +
-                    "\t \t \t  Balance: ${:.2f}".format(balance)
-                    )
+        feedback = statement_gen("^^ Sorry. You got a horse. You won back 50c from your $1 ^^ ", "^") 
+
     else:
         balance -= 0.5
-        Rounds_played += 1
-        feedback = ("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ \n"
-                    "^^ Sorry. You got a horse. You won back 50c from your $1 ^^ \n"
-                    "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ \n"
-                    "Rounds played: {}".format(Rounds_played) +
-                    "\t \t \t  Balance: ${:.2f}".format(balance)
-                    )
+        feedback = statement_gen("^^ Sorry. You got a zebra. You won back 50c from your $1 ^^ ", "^") 
 
     print()
-
     print(feedback)
+    print("Rounds played: {}".format(Rounds_played) +
+                    "\t \t \t  Balance: ${:.2f}".format(balance))
     print()
 
     if balance < 1:
